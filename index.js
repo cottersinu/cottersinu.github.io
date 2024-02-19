cf8a37fd2703e3bcfd0bbbc97e0b35a51f7a874b
cottersinu <154692338+cottersinu@users.noreply.github.com>
2024-02-19 10:15:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -52,3 +52,47 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//accordion
+var acc = document.getElementById("accordionBtn"); // Get the specific button
+
+acc.addEventListener("click", function() {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+    this.textContent = "Read more"; // Change text to "Read more"
+  } else {
+    panel.style.display = "block";
+    this.textContent = "Read less"; // Change text to "Read less"
+  }
+});
